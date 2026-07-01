3613527f08ea56028e87a670870eed7f56eefc37
Rathod <rathod74165@gmail.com>
2026-07-01 12:24:24 +0530

    fix: preserve line breaks in question text rendering

--- lib/renderMath.ts
+++ lib/renderMath.ts
@@ -202,5 +202,6 @@ function escapeHtml(text: string): string {
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
     .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
+    .replace(/'/g, "&#039;")
+    .replace(/\n/g, "<br>");
 }
